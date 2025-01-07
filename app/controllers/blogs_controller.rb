3772207552aeaa3ac549a69e4590d434af478a97
Kamrul Islam Shahin <shahin.cuetcse@gmail.com>
2025-01-07 18:39:46 +0600

    add turbo stream in form and index

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -25,6 +25,7 @@ class BlogsController < ApplicationController
 
     respond_to do |format|
       if @blog.save
+        format.turbo_stream
         format.html { redirect_to @blog, notice: "Blog was successfully created." }
         format.json { render :show, status: :created, location: @blog }
       else
